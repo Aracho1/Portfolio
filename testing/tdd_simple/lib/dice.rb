23072d02cb72801facc9e99b5d0e2e41347ece53
Ara <hello.ara@pm.me>
2020-11-25 15:23:20 +0900

    update roll method

--- testing/tdd_simple/lib/dice.rb
+++ testing/tdd_simple/lib/dice.rb
@@ -1,5 +1,15 @@
 class Dice
-  def roll
-    p 'roll!'
+
+  def roll(n=1)
+    array = []
+    if n == 1
+      array = rand(1..6)
+    else
+      n.times{ array << rand(1..6) }
+      array.join(",")
+    end
   end
 end
+
+dice = Dice.new
+p dice.roll(3)
--- testing/tdd_simple/spec/dice_spec.rb
+++ testing/tdd_simple/spec/dice_spec.rb
@@ -5,4 +5,21 @@ describe Dice do
   it 'roll a dice' do
     expect(dice).to respond_to(:roll)
   end
+
+  describe '#roll'do
+    it 'should give a random number between 1 and 6' do
+      allow(dice).to receive(:roll).and_return(6)
+      expect(dice.roll).to eq 6
+    end
+
+    it 'should not give a number other than between 1 and 6' do
+      expect(dice.roll).not_to eq 7
+      expect(dice.roll).not_to eq 0
+    end
+
+    it 'should be able to roll multiple dices' do
+      allow(dice).to receive(:roll).and_return("3,4,5")
+      expect(dice.roll(3)).to eq ("3,4,5")
+    end
+  end
 end
