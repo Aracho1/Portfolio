fa8dd02abb8d92a12f22069b35d3b279502ceb65
Ara <hello.ara@pm.me>
2020-11-27 21:13:36 +0900

    add receipt method

--- testing/simple_checkout/lib/checkout.rb
+++ testing/simple_checkout/lib/checkout.rb
@@ -1,5 +1,37 @@
-class Item
-  def price
-    
+class Supermarket
+  attr_accessor :shelf
+  attr_reader :basket
+
+  def initialize
+    @shelf = {"apple" => 1.20, "banana" => 0.8, "oranges" => 3.2, "chocolate" => 2.4}
+    @basket = [{"juice" => 2.4}, {"gum" => 0.8}]
+  end
+
+  # show price of an item
+  def price(item)
+    @shelf.select{ |name,price| name == item }.values[0]
+  end
+
+  # scan an item and put in in the basket
+  def scan(item)
+    if !shelf.include?(item)
+      "item not available for scan."
+    else
+      @basket << @shelf.select{ |name,price| name == item }
+    end
+  end
+
+  # show the total price of all items in the basket
+  def basket_total
+    sum = @basket.sum{ |item| item.values[0] }
+  end
+
+  # show basket items and prices in formatted way
+  def receipt
+    @basket.each do |item|
+      item.each do |name, price|
+        puts "item: #{name}, price: £#{ "%.2f" % item.values[0]}"
+      end
+    end
   end
 end
--- testing/simple_checkout/spec/checkout_spec.rb
+++ testing/simple_checkout/spec/checkout_spec.rb
@@ -1,9 +1,31 @@
 require 'checkout'
 
-Describe Item do
-  subject(:apple) {Item.new}
-  describe "#price" do
-    it 'prints out the price'
-    expect(apple.price).to eq(3)
+describe Supermarket do
+  describe "#price(item)" do
+    it 'shows the price of the item' do
+      expect(subject.price("apple")).to eq(1.20)
+    end
+  end
+
+  describe "#scan(item)" do
+    it 'stores the item in the scan array' do
+      expect{ subject.scan("apple") }.to change { subject.basket }
+    end
+
+    it "returns 'item not available' when an invalid item is scanned" do
+      expect(subject.scan("pineapple")).to eq("item not available for scan.")
+    end
+  end
+
+  describe "#baset_total" do
+    it 'shows the total price all scanned items' do
+      expect(subject.basket_total).to eq(3.2)
+    end
+  end
+
+  describe "#receipt" do
+    it 'shows the formatted price of each item in the basket' do
+      expect(subject.receipt).to eq("Item: gum, price: £0.80")
+    end
   end
 end
