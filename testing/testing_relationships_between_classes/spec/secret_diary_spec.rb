c1e992029d270c4a71be6d70b4b9b45e86d7f0f5
Ara <hello.ara@pm.me>
2020-12-01 13:39:55 +0900

    update testing_between_classes exercise

--- testing/testing_relationships_between_classes/spec/secret_diary_spec.rb
+++ testing/testing_relationships_between_classes/spec/secret_diary_spec.rb
@@ -23,12 +23,14 @@ RSpec.describe SecretDiary do
       diary = double("diary")
       allow(diary).to receive(:lock)
       expect(diary).not_to receive(:read)
+      # shouldn't I make the test to return "Go away!"?
     end
 
     it 'refuses to be written' do
       diary = double("my secret diary")
       allow(diary).to receive(:lock)
       expect(diary).not_to receive(:write)
+      # shouldn't I make the test to return "Go away!"?
     end
   end
 
@@ -37,14 +39,16 @@ RSpec.describe SecretDiary do
       diary = double("my secret diary")
       allow(diary).to receive(:unlock)
       expect(diary).to receive(:read)
-      diary.read
+      diary.read # why is this line needed to pass the test?
+      # why does this return nil and not "my secret diary"?
+      # what can I do to make this return the diary string?
     end
 
     it 'gets written' do
       diary = double("my secret diary")
       allow(diary).to receive(:unlock)
       expect(diary).to receive(:write)
-      diary.write("my new diary")
+      diary.write("my new diary") # why is this line needed to pass the test?
     end
   end
 end
